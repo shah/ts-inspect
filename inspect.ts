a35614a238234cbbb27f19e31de535afc642cff6
Shahid N. Shah <shahid@shah.org>
2020-10-21 21:30:56 -0400

    Eliminate duplicates from issue recorder

--- inspect.ts
+++ inspect.ts
@@ -4,13 +4,18 @@ import {
   isInspectionDiagnostics,
 } from "./context.ts";
 import {
+  InspectionIssue,
   isDiagnosable,
   isInspectionException,
   isInspectionIssue,
   isInspectionIssuesManager,
-  mergeIssuesIntoResult,
 } from "./issue.ts";
-import { EmptyInspectorsResult, InspectionResult } from "./result.ts";
+import {
+  EmptyInspectorsResult,
+  InspectionResult,
+  isInspectionResult,
+} from "./result.ts";
+import { safety } from "./deps.ts";
 
 export interface Inspector<T> {
   (
@@ -24,6 +29,52 @@ export interface InspectionPipe<T> {
     initTarget: T,
     ctx?: InspectionContext,
   ): Promise<T | InspectionResult<T>>;
+}
+
+export interface InspectorProvenance<T> {
+  readonly inspector: Inspector<T>;
+  readonly inspectorIdentity: string;
+}
+
+export const InspectorProvenance = safety.typeGuard<
+  InspectorProvenance<unknown>
+>(
+  "inspector",
+  "inspectorIdentity",
+);
+
+export interface InspectorProvenanceSupplier<T> {
+  readonly inspectorProvenance: InspectorProvenance<T>;
+}
+
+export const isInspectorProvenanceSupplier = safety.typeGuard<
+  InspectorProvenanceSupplier<unknown>
+>("inspectorProvenance");
+
+export function decorateProvenance<T>(
+  target: T | InspectionResult<T>,
+  inspector: Inspector<T>,
+): T | (InspectionResult<T> & InspectorProvenanceSupplier<T>) {
+  if (isInspectionResult<T>(target)) {
+    return {
+      ...target,
+      inspectorProvenance: {
+        inspector: inspector,
+        inspectorIdentity: inspector.name,
+      },
+    };
+  }
+  return target;
+}
+
+export interface Encounterable {
+  readonly encounteredBy: symbol;
+}
+
+export const isEncountered = safety.typeGuard<Encounterable>("encounteredBy");
+
+export function isEncounteredBy(o: unknown, by: symbol): o is Encounterable {
+  return isEncountered(o) && o.encounteredBy === by;
 }
 
 export function inspectionPipe<T, D, E extends Error>(
@@ -42,11 +93,12 @@ export function inspectionPipe<T, D, E extends Error>(
       return empty;
     }
 
+    const pipeID = Symbol();
     let result: T | InspectionResult<T> = inspectionTarget;
     const diags = isInspectionDiagnostics<T, E>(ctx) ? ctx : undefined;
     for (const inspect of inspectors) {
       try {
-        result = await inspect(result, ctx);
+        result = decorateProvenance(await inspect(result, ctx), inspect);
         if (isInspectionException<T, E>(result)) {
           if (diags) {
             result = await diags.onException(
@@ -56,13 +108,28 @@ export function inspectionPipe<T, D, E extends Error>(
           }
           if (!diags || !diags?.continue(result)) return result;
         } else if (isInspectionIssue<T>(result)) {
-          if (isInspectionIssuesManager<T>(result)) {
-            if (diags) {
-              for (const issue of result.inspectionIssues) {
-                diags.onIssue(issue);
+          if (diags && isInspectionIssuesManager<T>(result)) {
+            // if the issues are being collected by the result, then let's
+            // make sure we don't duplicate the onIssue() calls across
+            // inspectors
+            for (let i = 0; i < result.inspectionIssues.length; i++) {
+              const issue = result.inspectionIssues[i];
+              if (isEncounteredBy(issue, pipeID)) {
+                continue;
               }
+              const ei:
+                & InspectionIssue<T>
+                & Encounterable
+                & InspectorProvenanceSupplier<T> = {
+                  ...issue,
+                  encounteredBy: pipeID,
+                  inspectorProvenance: {
+                    inspector: inspect,
+                    inspectorIdentity: inspect.name,
+                  },
+                };
+              result.inspectionIssues[i] = await diags.onIssue(ei);
             }
-            result = mergeIssuesIntoResult<T>(result, result);
           } else if (
             diags && isInspectionIssue<T>(result) && isDiagnosable<D>(result)
           ) {
